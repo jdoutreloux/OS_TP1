534b6ba3b294cfaa60625d727f05e1cda59ec0b9
jdoutreloux <justine.doutreloux@hotmail.be>
2017-09-24 15:27:37 +0200

    ajout des premiers fichiers

---
+++ serveur.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include "serveur.h"
+
+int main( int argc, const char* argv[] )
+{
+	int msgId;
+	msgId = msgget(10,0666);
+	
+	*msg //creer variable pour stocker le message
+	//creer la structure pour le message client
+	//gerer pour le while true
+	msgrcv(msgId,  msg,sizeof(struct...),1,0666);
+	*answer //creer message reponse
+	int i = msgsnd(msgId, answer, sizeof(answer), 0666);
+	
+	
+}
