1c6a97aa5b77908e7f32ee7240508dc6c214a15f
jdoutreloux <justine.doutreloux@gmail.com>
2017-09-29 13:36:02 +0200

    modif serveur.c

--- serveur.c
+++ serveur.c
@@ -8,11 +8,14 @@ int main( int argc, const char* argv[] )
 	int msgId;
 	msgId = msgget(10,0666);
 	
-	void* msg; //creer variable pour stocker le message
+	struct message* msg; //creer variable pour stocker le message
 	struct data d; //creer la structure pour le message client
 	//gerer pour le while true
 	msgrcv(msgId,  msg,sizeof(struct...),1,0666);
-	*answer //creer message reponse
+	d = msg->data;
+	struct message* answer; //creer message reponse
+	answer->type = data->pid;
+	//answer->  2e structure message?
 	int i = msgsnd(msgId, answer, sizeof(answer), 0666);
 	
 	
