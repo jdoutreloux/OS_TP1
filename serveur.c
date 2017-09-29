ac2b87a83c75b50aa40920260e07daaf05308e5b
lougerard <l.gerard@students.ephec.be>
2017-09-29 14:18:32 +0200

    change struct

--- serveur.c
+++ serveur.c
@@ -8,12 +8,12 @@ int main( int argc, const char* argv[] )
 	int msgId;
 	msgId = msgget(10,0666);
 	
-	struct message* msg; //creer variable pour stocker le message
+	struct receive msg; //creer variable pour stocker le message
 	struct data d; //creer la structure pour le message client
 	//gerer pour le while true
 	msgrcv(msgId,  msg,sizeof(struct...),1,0666);
 	d = msg->data;
-	struct message* answer; //creer message reponse
+	struct receive* answer; //creer message reponse
 	answer->type = data->pid;
 	//answer->  2e structure message?
 	int i = msgsnd(msgId, answer, sizeof(answer), 0666);
