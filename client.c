8388c7cc5b86e00eea730eb1de1474da481dc49d
lougerard <l.gerard@students.ephec.be>
2017-10-02 14:25:51 +0200

    correction client.c

--- client.c
+++ client.c
@@ -8,9 +8,15 @@ int main( int argc, const char* argv[] )
 {
         int msgId;
         msgId = msgget(10,0666);
+	
+	struct receive* rcv;
+	rcv->msgdata =argv[0];
+	rcv->mtype =1;
 
-        const void *filename = argv[0];
-	int i = msgsnd(msgId, filename,sizeof(filename), 0666);
+        //const void *filename = argv[0];
+	if (rcv->msgdata != NULL) {
+		int i = msgsnd(msgId, rcv,sizeof(filename), 0666);
+	}
 
 	msgrcv(msgId,  msg,sizeof(...),1,0666);
 }
